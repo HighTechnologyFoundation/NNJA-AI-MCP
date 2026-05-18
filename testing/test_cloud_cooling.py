32ecad437ed6a9edb5f8aa42101d90a657543fc9
Shane Thoma <sthoma@wvhtf.org>
2026-05-18 15:45:19 -0400

    Added simple tests to check basic functionality of data analysis tools

---
+++ testing/test_cloud_cooling.py
@@ -0,0 +1,28 @@
+from fastmcp import Client
+import asyncio
+
+mcp_client = Client("http://localhost:8000/mcp")
+
+
+async def main():
+    async with mcp_client:
+        print(f"Connected: {mcp_client.is_connected()}")
+
+        res = await mcp_client.call_tool(
+            "calculate_spectral_index",
+            {
+                "dataset": "seviri-sevasr-NC021042",
+                "time": "2024-11-20",
+                "index_name": "cloud_cooling",
+                "lat_bounds": [35, 50],
+                "lon_bounds": [-5, 15],
+            },
+        )
+
+        print(res)
+
+
+
+# Run the client when this Python file runs
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ testing/test_lapse_rate.py
@@ -0,0 +1,28 @@
+from fastmcp import Client
+import asyncio
+
+mcp_client = Client("http://localhost:8000/mcp")
+
+
+async def main():
+    async with mcp_client:
+        print(f"Connected: {mcp_client.is_connected()}")
+
+        res = await mcp_client.call_tool(
+            "calculate_lapse_rate",
+            {
+                "time": "2023-07-16",
+                "lat_bounds": [30, 40],   # desert: 30, 40 | tropical: 10, 30
+                "lon_bounds": [-120, -110], # desert: -120, -110 | tropical: -85, -50
+                "level1_hpa": 925, # 1000 | 1000 | 925 | 850
+                "level2_hpa": 850, #  850 |  500 | 850 | 500
+            },
+        )
+
+        print(res)
+
+
+
+# Run the client when this Python file runs
+if __name__ == "__main__":
+    asyncio.run(main())
