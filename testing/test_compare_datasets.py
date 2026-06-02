ca4cae6de603e1675b29aa569bedcd4082619d3f
Shane Thoma <sthoma@wvhtf.org>
2026-06-02 09:13:16 -0400

    Added simple timing to `test_compare_datasets.py`

--- testing/test_compare_datasets.py
+++ testing/test_compare_datasets.py
@@ -10,17 +10,22 @@ async def main():
     async with mcp_client:
         print(f"Connected: {mcp_client.is_connected()}")
 
+        start = time.time()
+
         res = await mcp_client.call_tool(
             "compare_datasets",
             {
-                "datasets": ["amsua-1bamua-NC021023", "atms-atms-NC021203"],
+                "datasets": ["amsua-1bamua-NC021023", "atms-atms-NC021203", "mhs-1bmhs-NC021027"],
                 "time": "2023-01-01",
-                "end_time": "2023-01-14",
+                "end_time": "2023-01-02",
                 "variables": ["brightness temperature"],
                 "lat_bounds": [30, 40],
                 "lon_bounds": [-120, -110],
             },
         )
+
+        end = time.time()
+        print(f"Time taken: {end - start:.2f} seconds")
 
         print(res)
 
