aa664f45d25fb225771c295c4adb04b616eacb42
Shane Thoma <sthoma@wvhtf.org>
2026-06-24 11:30:13 -0400

    test: add integration tests for load_data_sample & metadata tools (#59)
    Adds integration tests to help with regression testing for various
    lightweight metadata tools and `load_data_sample`.

---
+++ testing/integration_load_data_sample.py
@@ -0,0 +1,13 @@
+from _client import run_tool
+
+run_tool(
+    "load_data_sample",
+    {
+        "dataset": "amsua-1bamua-NC021023",
+        "time": "2023-07-01",
+        "variables": ["brightness temperature"],
+        "lat_bounds": [30, 40],
+        "lon_bounds": [-120, -110],
+        "rows": 10,
+    },
+)
