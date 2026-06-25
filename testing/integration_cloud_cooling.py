33aa0cc9f146292d273829144f07ef0ab7213586
Shane Thoma <sthoma@wvhtf.org>
2026-06-25 12:01:35 -0400

    docs(test): label integration tests' lat/long regions
    Added comments noting the general regions captured in the LAT/LON bounds
    used in the integration tests.

--- testing/integration_cloud_cooling.py
+++ testing/integration_cloud_cooling.py
@@ -6,7 +6,7 @@ run_tool(
         "dataset": "seviri-sevasr-NC021042",
         "time": "2024-11-20",
         "index_name": "cloud_cooling",
-        "lat_bounds": [35, 50],
+        "lat_bounds": [35, 50],  # Western Europe
         "lon_bounds": [-5, 15],
         "end_time": "2024-11-21",
     },
